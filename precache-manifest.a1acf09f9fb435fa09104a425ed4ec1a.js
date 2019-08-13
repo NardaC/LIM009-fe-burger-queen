8584a28af89d61348aa82eb4e3031eac9a4e4d5b
NardaC <nardac6@gmail.com>
2019-08-13 18:12:59 -0500

    Updates

---
+++ precache-manifest.a1acf09f9fb435fa09104a425ed4ec1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a669dc712379a61f314275429579ea74",
+    "url": "/LIM009-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "c6971d0ea19276f8099a",
+    "url": "/LIM009-fe-burger-queen/static/css/main.b9304904.chunk.css"
+  },
+  {
+    "revision": "aeaa1355069f313c1d19",
+    "url": "/LIM009-fe-burger-queen/static/js/2.2f567c1d.chunk.js"
+  },
+  {
+    "revision": "c6971d0ea19276f8099a",
+    "url": "/LIM009-fe-burger-queen/static/js/main.5361af46.chunk.js"
+  },
+  {
+    "revision": "4ec68e84b8b177095eae",
+    "url": "/LIM009-fe-burger-queen/static/js/runtime~main.63907d42.js"
+  }
+]);
