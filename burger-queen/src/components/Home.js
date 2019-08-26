2c6b7be9c1533ef165099777da4a3948695962d6
NardaC <nardac6@gmail.com>
2019-08-25 19:32:04 -0500

    editando home

--- burger-queen/src/components/Home.js
+++ burger-queen/src/components/Home.js
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div className="wrap">
       
-        <Link to="/cocinero" className="mt-5">
+        <Link to="/cocinero" className="m-5">
           <img src="https://i.postimg.cc/1Xqk1ttr/cocinero.png" />
         </Link>
      
-        <Link to="/mesero" className="mt-5">
+        <Link to="/mesero" className="m-5">
           <img src="https://i.postimg.cc/vBjJK9Qm/mesero.png" />
         </Link>
       
